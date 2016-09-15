dbcda0a8a6ef4b947fea22d7230557e5affbf52e
cesar <cesar@CERCIBER>
2016-09-14 22:35:06 -0500

    Imprimir en pantalla

--- src/capturalabandera/CapturaLaBandera.java
+++ src/capturalabandera/CapturaLaBandera.java
@@ -4,8 +4,7 @@ public class CapturaLaBandera {
 
     public static void main(String[] args) {
         
-        
-        
+        System.out.println("Captura la bandera proooo :D"); 
         
     }
     
