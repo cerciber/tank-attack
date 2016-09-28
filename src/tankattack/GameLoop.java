889de39147fcf207ec5548032d24b5370661f430
cesar <cesar@CERCIBER>
2016-09-27 21:58:42 -0500

    Animacion

--- src/tankattack/GameLoop.java
+++ src/tankattack/GameLoop.java
@@ -1,13 +1,17 @@
 package tankattack;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 public class GameLoop extends JPanel implements Runnable{
     
-    int pausaDeTiempo = 1000;   
+    int pausaDeTiempo = 1;   
     MenuPrincipal menuPrincipal = new MenuPrincipal(); 
+    
+    int x = 0,y = 0;
+    boolean dirX = true, dirY = true;
     
     // Metodo para pintar elementos en pantalla
     @Override
@@ -16,6 +20,10 @@ public class GameLoop extends JPanel implements Runnable{
         super.paint(g);
         Graphics2D g2 = (Graphics2D) g;
         
+        g.setColor (Color.BLACK);
+        g.fillRect (0, 0, 800, 600);
+        g.setColor (Color.yellow);
+        g.fillOval (x, y, 200, 200);
     }
     
     // Metodo para gestionar acciones del mouse y el teclado
@@ -28,6 +36,12 @@ public class GameLoop extends JPanel implements Runnable{
     // Metodo para actualizarlos datos del juego
     public void actualizar(){
         
+        if(x < 0){dirX = true;}
+        if(y < 0){dirY = true;}
+        if(x > 600){dirX = false;}
+        if(y > 400){dirY = false;}
+        if(dirX){x++;}else{x--;}
+        if(dirY){y++;}else{y--;}
         
         
     }
