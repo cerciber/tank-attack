67ac7f9c40d90d6fe75048b2098260904d07995f
cesar <cesar@CERCIBER>
2016-10-01 15:35:04 -0500

    Comentarios detallados de las clases Gameloop y TankAttack

--- src/tankattack/GameLoop.java
+++ src/tankattack/GameLoop.java
@@ -1,5 +1,6 @@
 package tankattack;
 
+// Librerias
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -10,71 +11,91 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import javax.swing.JPanel;
 
+/* Clase GameLoop que permite:
+    - Visualizar graficos a la ventana
+    - Reponder a eventos del mouse y el teclado
+    - Actualizar infomracion cada periodo de tiempo
+*/
 public class GameLoop extends JPanel implements Runnable{
     
-    // peridodo de pausa del gameloop en milisegundos
-    int pausaDeTiempo = 1;   
+    /* Variables globales */
+    int pausaDeTiempo = 1;   // Peridodo de pausa del gameloop en milisegundos
     
-    // crear  objeto bandera
-    Bandera bandera = new Bandera(); 
+    /* Objetos involucrados */
+    MenuPrincipal menuPrincipal = new MenuPrincipal(); // crear objeto Menu Principal
     
-    // Metodo para pintar elementos en pantalla
+    /* Metodo para Visualizar graficos a la ventana 
+       - Este metodo viene de la clase padre JPanel
+    */
     @Override
     public void paint(Graphics g){
         
-        super.paint(g);
-        Graphics2D g2 = (Graphics2D) g;
+        /* Obtener obejeto grafico del Panel */
+        super.paint(g);  // Asignar objeto que se pintará
+        Graphics2D g2 = (Graphics2D) g; //Crear objeto Graphics2D para acceder a mas propiedades de los graficos
         
         // Pintar fondo blanco de acuerdo al tamaño de la ventana
         g2.setColor (Color.white);
         g2.fillRect(0, 0, this.getWidth(), this.getWidth());
         
-        // Pintar bandera
-        bandera.paint(g2);
+        // Pintar menu Principal
+        menuPrincipal.paint(g2);
         
     }
     
-    // Metodo para gestionar acciones del mouse y el teclado
+    /* Metodo para gestionar acciones del mouse y el teclado */
     public void eventos(){
         
+        /* Detectar acciones del teclado en la ventana */
         this.getParent().addKeyListener(new KeyListener() {
 
+            /* Detectar cuando se tipea una letra */
             @Override
             public void keyTyped(KeyEvent ke) {}
 
+            /* Detectar cuando se presiona una letra */
             @Override
             public void keyPressed(KeyEvent ke) {}
 
+            /* Detectar cuando se suelta una letra */
             @Override
             public void keyReleased(KeyEvent ke) {}
            
         });
 
+        /* Detectar movimiento del mouse en la ventana */
         this.getParent().addMouseMotionListener(new MouseMotionListener() {
 
+            /* Detectar movimiento del mouse arrastrado */
             @Override
             public void mouseDragged(MouseEvent me) {}
 
+            /* Detectar movimiento del mouse sin presionar */
             @Override
             public void mouseMoved(MouseEvent me) {}
 
         });
         
-        
+        /* Detectar acciones del mouse en la ventana */
         this.getParent().addMouseListener(new MouseListener() {
 
+            /* Detectar click del mouse */
             @Override
             public void mouseClicked(MouseEvent me) {}
 
+            /* Detectar cuando se presiona una tecla del mouse */
             @Override
             public void mousePressed(MouseEvent me) {}
 
+            /* Detectar cuando se suelta una tecla del mouse */
             @Override
             public void mouseReleased(MouseEvent me) {}
 
+            /* Detectar cuando el mouse entra en la ventana */
             @Override
             public void mouseEntered(MouseEvent me) {}
 
+            /* Detectar cuando el mouse sale de la ventana */
             @Override
             public void mouseExited(MouseEvent me) {}
             
@@ -82,30 +103,32 @@ public class GameLoop extends JPanel implements Runnable{
         
     } 
     
-    // Metodo para actualizarlos datos del juego
+    // Metodo para actualizar información del juego
     public void actualizar(){
-        
-
+    
     }
 
-    // Metodo para pausar el hilo por un lapso de tiempo determinado
+    // Metodo para pausar el hilo por un periodo de tiempo determinado
     public void Esperar() {
     
         try{Thread.sleep(pausaDeTiempo);}
         catch(InterruptedException ex){}
     }
     
-    // Metodo para ejecutar los metodos anteriores indefinidamente
+    /* Metodo para ejecutar los metodos anteriores indefinidamente
+       - Este metodo viene de la clase implemetada Runnable
+    */
     @Override
     public void run() {
         
-        eventos();
+        eventos();  // Llamar metodo eventos
         
+        /* Repetir llamado de eventos indefinidamente */
         while(true){
             
-            actualizar();
-            repaint();
-            Esperar();
+            actualizar();  // Llamar metodo actualizar
+            repaint();  // Llamar metodo paint
+            Esperar();  // Llamar metodo esperar
             
         }
         
--- src/tankattack/MenuPrincipal.java
+++ src/tankattack/MenuPrincipal.java
@@ -1,6 +1,17 @@
 
 package tankattack;
 
+import java.awt.Graphics2D;
+
 public class MenuPrincipal {
+    
+    int ancho;
+    int alto;
+    
+    
+    public void paint(Graphics2D g){
+        
+        
+    }
     
 }
