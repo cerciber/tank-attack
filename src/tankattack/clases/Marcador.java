4c935c4186dad79e7cddf0964ec06b772621d1d2
FAMILIA <FAMILIA@PC-HOME>
2016-11-08 22:36:31 -0500

    Actualizacion: Nombre jugadores

--- src/tankattack/clases/Marcador.java
+++ src/tankattack/clases/Marcador.java
@@ -5,6 +5,7 @@ import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import javax.swing.JOptionPane;
 
 public class Marcador {
     
@@ -16,12 +17,17 @@ public class Marcador {
     int y = 10;
     int w = 800 - 20;
     int h = 100 - 20;
-    
+    int Puntaje1 = 0;
+    int Puntaje2 = 0;
+    String J1;
+    String J2;
+   
     /* Fuente texto */
     Font fuente = new Font ("Helvetica", Font.BOLD, 12);
     
     public Marcador(VS2Player e){
-        
+        J1=JOptionPane.showInputDialog(null,"Nombre primer jugador");
+        J2=JOptionPane.showInputDialog(null,"Nombre segundo jugador");
         escenario = e;
         
     }
@@ -35,11 +41,13 @@ public class Marcador {
         g.fillRect(x + 10, y + 10, w - 20, h - 20);
         g.setColor(Color.white);
         g.setFont(fuente);
-        g.drawString("Jugador 1", x + 30, y + g.getFontMetrics().getAscent() + 30);
-        g.drawString("Jugador 2", x + w - 30 - g.getFontMetrics()
-                .stringWidth("Jugador 2"), y + g.getFontMetrics().getAscent() + 30);
+        g.drawString(J1+": "+Puntaje1, x + 30, y + g.getFontMetrics().getAscent() + 30);
+        
+        g.drawString (J2+": "+Puntaje2, x + w - 30 - g.getFontMetrics()
+                .stringWidth(J2), y + g.getFontMetrics().getAscent() + 30);
          
     }
+    
     
     public void eventos(MouseEvent me){
         
