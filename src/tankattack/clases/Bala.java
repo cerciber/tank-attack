91a6aa69217634a95b034988853c5590cca7a038
cesar <cesar@CERCIBER>
2016-11-26 13:47:37 -0500

    Creacion de muros sin encierros

---
+++ src/tankattack/clases/Bala.java
@@ -0,0 +1,130 @@
+package tankattack.clases;
+
+import java.awt.Image;
+import java.awt.Rectangle;
+import javax.swing.ImageIcon;
+
+
+public class Bala {
+    
+    /* Objeto contenedor */
+    Tanque tank;
+    
+    Image Bala;
+    int x;
+    int y;
+    int direccion;
+    int SPEEDX;
+    int SPEEDY;
+    int SPEEDFINAL = 18;
+    boolean visible;
+    static int impactoT1 = 0;
+    static int impactoT2 = 0;
+    public Bala(Tanque tank, int x, int y, int direcc) {
+        
+        Bala = new ImageIcon(this.getClass()
+                .getResource("/tankattack/imagenes/tanque/bala.png")).getImage();
+        
+        this.tank = tank;
+        visible = true;
+        this.x = x;
+        this.y = y;
+        this.direccion = direcc;
+        choice();
+           
+    }
+    
+    public int getX(){
+      return x;
+    }
+
+    public int getY(){
+      return y;
+    }
+
+    public Image getImage(){
+      return Bala;
+    }
+    
+    public boolean isVisible(){
+      return visible;
+    }
+
+    public void setVisible(boolean visible){
+      this.visible = visible;
+    }
+    public void update(){
+        
+        Rectangle rectanglePlayer = getBounds(direccion);
+        if(y>Tanque.BordeInferior||y<Tanque.BordeSuperior||x>Tanque.BordeDerecho||x<Tanque.BordeIzquierdo) visible = false;
+        for(int i = 0; i <= 13; i++){
+                for(int j = 0; j <= 9; j++){
+         
+                    if (rectanglePlayer.intersects(Tablero.muros[i][j].getx(), Tablero.muros[i][j].gety(), 50, 50)) {
+                        
+                        switch(Tablero.muros[i][j].getname()){
+                        case "ladrillo":
+                            Tablero.muros[i][j].impactos++;
+                            visible=false; 
+                            break;
+                        case "piedra":
+                            Tablero.muros[i][j].impactos++;
+                            visible=false; 
+                            break;    
+                        case "acero":
+                            Tablero.muros[i][j].impactos++;
+                            visible=false; 
+                            break;    
+                        }
+                        
+                    }
+                       
+                }
+        }
+        if (rectanglePlayer.intersects(tank.tablero.tanque1.getx(), tank.tablero.tanque1.gety(), 50, 50)) {
+            visible = false;
+            System.out.println("Impactado tanque 1");
+            impactoT1++;
+        }
+        if (rectanglePlayer.intersects(tank.tablero.tanque2.getx(), tank.tablero.tanque2.gety(), 50, 50)) {
+            visible = false;
+            System.out.println("Impactado tanque 2");
+            impactoT2++;
+        }
+        y+= SPEEDY;
+        x+=SPEEDX;
+    }
+    public final void choice(){
+        if(direccion==1){SPEEDX=0;SPEEDY=-SPEEDFINAL;}
+        if(direccion==2){SPEEDX=0;SPEEDY=SPEEDFINAL;}
+        if(direccion==3){SPEEDX=-SPEEDFINAL;SPEEDY=0;}
+        if(direccion==4){SPEEDX=SPEEDFINAL;SPEEDY=0;}
+        if(direccion==0){this.visible=false;}
+    }
+    
+    public int getdir(){
+      return direccion;
+    }
+    public Rectangle getBounds(int i) {
+        switch (i){
+            case 1:
+                return new Rectangle(x, y-48, 20, 20);
+            
+            case 2:
+                return new Rectangle(x, y+68, 20, 20);
+                
+            case 3:
+                return new Rectangle(x-48, y, 20, 20);
+                
+            case 4:
+                return new Rectangle(x+68, y, 20, 20);
+            
+                    
+            default: return new Rectangle(x, y, 20, 20);
+        }
+        
+    }
+    
+}
+    
+
---
+++ src/tankattack/clases/MenuPrincipal.java
@@ -0,0 +1,106 @@
+package tankattack.clases;
+
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+public class MenuPrincipal {
+    
+    /* Objeto contenedor */
+    PantallaDePresentacion pantallaDePresentacion;
+    
+    /* Objetos involucrados */
+    Configuracion configuracion;
+    VS2Player vs2Player;
+    
+    /* Cambiar pantalla */
+    // 0 = Permanecer
+    // 1 = Escenario
+    // 2 = Configuracion
+    int cambiarPantalla = 0;
+    
+    /* Subobjetos */
+    Objeto textoVS2Player = new Objeto(800/2-300/2, 300-50/2-50, 300, 50, 
+            "/tankattack/imagenes/menuPrinicipal/VS2Player.png");
+    Objeto textoConfiguracion = new Objeto(800/2-300/2, 300-50/2+50, 300, 50, 
+            "/tankattack/imagenes/menuPrinicipal/Configuracion.png");
+    
+    public MenuPrincipal(PantallaDePresentacion p){
+        
+        pantallaDePresentacion = p;
+        
+    }
+    
+    /* Pintar en pantalla */
+    public void paint(Graphics2D g){
+        
+        switch(cambiarPantalla){
+            case 0:
+                pantallaDePresentacion.fondo.paint(g);  // Pintar fondo
+                pantallaDePresentacion.titulo.paint(g);  // Pintar titulo
+                textoVS2Player.paint(g);  // Pintar texto VS 2 Player
+                textoConfiguracion.paint(g);  // Pintar texto Configuracion
+                break;
+            case 1:
+                vs2Player.paint(g);
+                break;
+            case 2:
+                configuracion.paint(g);
+                break;
+        }
+        
+    }
+    
+    public void eventos(MouseEvent me){
+        
+        switch(cambiarPantalla){
+            case 0:
+                if(textoVS2Player.click(me)){
+                    vs2Player = new VS2Player(this);
+                    cambiarPantalla = 1;
+                } else if(textoConfiguracion.click(me)){
+                    configuracion = new Configuracion(this);
+                    cambiarPantalla = 2;
+                }
+                break;
+            case 1:
+                vs2Player.eventos(me);
+                break;
+            case 2:
+                configuracion.eventos(me);
+                break;
+        }
+        
+    }
+    
+    public void eventos(KeyEvent ke){
+        
+        switch(cambiarPantalla){
+            case 1:
+                vs2Player.eventos(ke);
+                break;
+            case 2:
+                configuracion.eventos(ke);
+                break;
+        }
+        
+    }
+    
+    public void released(KeyEvent ke){
+        vs2Player.released(ke);
+    }
+    
+    public void actualizar(){
+        
+        switch(cambiarPantalla){
+            case 1:
+                vs2Player.actualizar();
+                break;
+            case 2:
+                configuracion.actualizar();
+                break;
+        }
+        
+    }
+    
+}
